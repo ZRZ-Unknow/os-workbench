dc6f65bee3401888eb3f52e5e6547e3e818fc681
tracer-nju <tracer@nju.edu.cn>
2020-02-16 20:33:53 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 8a5c04931ef146b8877043a452db51dc   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  20:33:53 up 42 min,  1 user,  load average: 0.71, 0.56, 0.58

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdint.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+
+#define pid_t int
+
+struct process
+{
+  pid_t pid;
+  pid_t ppid;
+  char state;
+  char name[64];
+  struct process *parent;
+  struct ChildList *children; 
+}root_proc={.pid=1,.ppid=0,.parent=NULL,.children=NULL};
+
+struct ChildList{
+  struct process *child;
+  struct ChildList *next;
+};
+
+bool HAV_V=false;
+bool HAV_N=false;
+bool HAV_P=false;
+struct process *root=&root_proc;
+
 
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
