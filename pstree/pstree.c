40d7d32159cbe6b39cb9a02dc279340220ba1fed
tracer-nju <tracer@nju.edu.cn>
2020-02-17 17:59:16 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: a234e0f179e342759e3d504c33a12801   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  17:59:16 up  4:12,  1 user,  load average: 0.88, 0.58, 0.83

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -158,9 +158,9 @@ int main(int argc, char *argv[]) {
     return 0;
   }
   search(root);
-  printf("start scan\n");
+  //printf("start scan\n");
   scan();
-  printf("end scan\n");
+  //printf("end scan\n");
   debugprint(root);
   return 0;
 }
