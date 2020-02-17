fa58ff5d3b089d97fff25b4f6c3e96255cd97dc1
tracer-nju <tracer@nju.edu.cn>
2020-02-17 20:43:44 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 5383d61050f140cfa4c657266255faa0   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  20:43:44 up 1 min,  1 user,  load average: 1.19, 0.59, 0.22

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -142,7 +142,7 @@ void scan(){
 void printBackup(struct process *proc){
   if(proc->parent!=NULL) printBackup(proc->parent);
   int print_len;
-  else print_len=(int)strlen(proc->name);
+  print_len=(int)strlen(proc->name);
   printf("%s%*s",(proc==root?"":(proc->children->next?" | ":"   ")),print_len,"");
 }
 void printTree(struct process *proc){
