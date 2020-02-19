12a8f274f5814d37e80b2fa0d4fe72bd01af2f65
tracer-nju <tracer@nju.edu.cn>
2020-02-19 22:26:53 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 35f3c3a5ee8b45f690e101eb6ad3e39d   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  22:26:53 up 47 min,  1 user,  load average: 0.80, 0.88, 0.82

--- amgame/include/game.h
+++ amgame/include/game.h
@@ -2,6 +2,23 @@
 #include <amdev.h>
 #include <klib.h>
 
+#define MAX_LEN 100
+
+struct Node{
+  int x;
+  int y;
+};
+struct Snake{
+  int dire;
+  int lenth;
+  struct Node node[MAX_LEN];
+};
+
+
+
+
+
+
 void splash();
 void print_key();
 static inline void puts(const char *s) {
