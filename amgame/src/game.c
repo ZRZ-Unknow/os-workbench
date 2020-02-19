3cb8e0613a36ead157a6dfef74c398b353a650d7
tracer-nju <tracer@nju.edu.cn>
2020-02-19 22:43:43 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 35f3c3a5ee8b45f690e101eb6ad3e39d   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  22:43:44 up  1:04,  1 user,  load average: 0.75, 0.59, 0.57

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -1,8 +1,30 @@
 #include <game.h>
-
+//贪吃蛇
 // Operating system is a C program!
+int w,h;
+struct Snake snake;
+void init_screen() {
+  _DEV_VIDEO_INFO_t info = {0};
+  _io_read(_DEV_VIDEO, _DEVREG_VIDEO_INFO, &info, sizeof(info));
+  w = info.width;
+  h = info.height;
+}
+void init_snake(){
+  snake.dire=UP;
+  snake.lenth=3;
+  snake.node[0].x=(w/SIDE/2)*SIDE,snake.node[0].y=(h/SIDE/2)*SIDE;
+  snake.node[1].x=(w/SIDE/2)*SIDE,snake.node[1].y=(h/SIDE/2)*SIDE-SIDE;
+  snake.node[2].x=(w/SIDE/2)*SIDE,snake.node[2].y=(h/SIDE/2)*SIDE-2*SIDE;
+}
+
+
+
+
+
 int main(const char *args) {
   _ioe_init();
+  init_screen();
+  init_snake();
   puts("mainargs = \"");
   puts(args); // make run mainargs=xxx
   puts("\"\n");
