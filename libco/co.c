bcd69ce0b69b756dd0125b018542fe1a1d4e7695
tracer-nju <tracer@nju.edu.cn>
2020-03-01 14:23:43 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 40818228a00a44a5a0f48c3dea8ec04f   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  14:23:43 up 22:27,  1 user,  load average: 0.73, 0.65, 0.55

--- libco/co.c
+++ libco/co.c
@@ -12,7 +12,25 @@ __attribute__((constructor)) void co_init() {
   co_main->prev=NULL;
 }
 struct co *co_generate(const char *name, void (*func)(void *), void *arg){
-  return NULL;
+  struct co *new_co=malloc(sizeof(struct co));
+  new_co->status=CO_NEW;
+  strcpy(new_co->name,name);
+  new_co->func=func;
+  new_co->arg=arg;
+  if(coroutines==NULL){
+    coroutines=new_co;
+    coroutines->next=coroutines;
+    coroutines->prev=coroutines;
+  }
+  else{
+    struct co* prev=coroutines->prev;
+    struct co* next=coroutines;
+    new_co->next=next;
+    next->prev=new_co;
+    new_co->prev=prev;
+    prev->next=new_co;
+  }
+  return new_co; 
 }
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
   co_current=co_generate(name,func,arg);
