1c739024f58d7ed42714dbdbb199cb980e4b0639
tracer-nju <tracer@nju.edu.cn>
2020-03-01 18:38:27 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 715533cc18264ac087ed77ac1d2a49f9   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  18:38:27 up 1 min,  1 user,  load average: 1.58, 0.60, 0.22

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -118,11 +118,11 @@ static void test_2() {
 }
 
 int main() {
-    //setbuf(stdout, NULL);
+    setbuf(stdout, NULL);
 
     //struct co *thd1=co_start(NULL,NULL,NULL);
     printf("Test #1. Expect: (X|Y){0, 1, 2, ..., 199}\n");
-    //test_1();
+    test_1();
 
     //printf("\n\nTest #2. Expect: (libco-){200, 201, 202, ..., 399}\n");
     //test_2();
