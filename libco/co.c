6bdea414e1fa7c5ec0f901ffa287bce20a074636
tracer-nju <tracer@nju.edu.cn>
2020-03-01 09:55:14 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 40818228a00a44a5a0f48c3dea8ec04f   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  09:55:14 up 17:58,  1 user,  load average: 1.74, 1.26, 1.06

--- libco/co.c
+++ libco/co.c
@@ -9,3 +9,12 @@ void co_wait(struct co *co) {
 
 void co_yield() {
 }
+static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
+  asm volatile (
+#if __x86_64__
+    "movq %0, %%rsp; movq %2, %%rdi; jmp *%1" : : "b"((uintptr_t)sp),     "d"(entry), "a"(arg)
+#else
+    "movl %0, %%esp; movl %2, 4(%0); jmp *%1" : : "b"((uintptr_t)sp - 8), "d"(entry), "a"(arg)
+#endif
+  );
+}
