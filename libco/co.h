bca1f75bc3275924d57c38aee6c979d3b73965a0
tracer-nju <tracer@nju.edu.cn>
2020-03-01 14:26:01 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 40818228a00a44a5a0f48c3dea8ec04f   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  14:26:01 up 22:29,  1 user,  load average: 0.86, 0.70, 0.57

--- libco/co.h
+++ libco/co.h
@@ -1,28 +1,3 @@
-#include <stdint.h>
-#include <stdio.h>
-#include <setjmp.h>
-#include <assert.h>
-#include <string.h>
-#include <stdlib.h>
-
-#define STACK_SIZE 4096
-enum co_status {
-  CO_NEW = 1, // 新创建，还未执行过
-  CO_RUNNING, // 已经执行过
-  CO_WAITING, // 在 co_wait 上等待
-  CO_DEAD,    // 已经结束，但还未释放资源
-};
-struct co {
-  char *name;
-  void (*func)(void *); // co_start 指定的入口地址和参数
-  void *arg;
-
-  enum co_status status;  // 协程的状态
-  jmp_buf        context; // 寄存器现场 (setjmp.h)
-  uint8_t        stack[STACK_SIZE]; // 协程的堆栈
-  struct co *next;
-  struct co *prev;
-};
 struct co* co_start(const char *name, void (*func)(void *), void *arg);
 void co_yield();
 void co_wait(struct co *co);
