4c55c2db5c7fe5dc0692db435113e140d2e739af
tracer-nju <tracer@nju.edu.cn>
2020-03-03 00:33:54 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: f9b38d59889849e19453a6ec47051e80   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  00:33:54 up  1:47,  1 user,  load average: 0.59, 0.67, 0.78

--- libco/co.c
+++ libco/co.c
@@ -169,7 +169,7 @@ void co_yield(){
         next->status=CO_RUNNING;
         Log("a new co %d start to run",next->id);
         PU(co_current->stackptr,next->stackptr);
-        printf("0x%p,0x%p",co_current,next);
+        printf("%d,%d",co_current->id,next-id);
         co_current=next;
         co_current->func(co_current->arg);
         co_current->status=CO_DEAD;
