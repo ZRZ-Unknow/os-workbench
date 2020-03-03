9749a473a2da29e4a460f05bbd33d5f1128cd371
tracer-nju <tracer@nju.edu.cn>
2020-03-03 09:47:23 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: f9b38d59889849e19453a6ec47051e80   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  09:47:23 up 11:00,  1 user,  load average: 0.89, 0.65, 0.54

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -141,7 +141,7 @@ int main() {
     co_yield();
     printf("\n\nTest #2. Expect: (libco-){200, 201, 202, ..., 399}\n");
     test_2();*/
-    for(int i=0;i<50;i++){
+    for(int i=0;i<100;i++){
         test_2();
         g_running=1;
     }
