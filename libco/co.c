62b231a7ebacb5b13423ad23ff0563898a9dcb6c
tracer-nju <tracer@nju.edu.cn>
2020-03-15 11:30:01 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: a864535bd1d5469ab2d1cb03dcf34f8f   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  11:30:01 up  1:22,  1 user,  load average: 1.35, 1.24, 1.27

--- libco/co.c
+++ libco/co.c
@@ -28,20 +28,8 @@ static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
 #endif
   );
 }
-#if defined(__i386__)
-  #define SP "%%esp"
-#elif defined(__x86_64__)
-  #define SP "%%rsp"
-#endif
-#define PUSH(sp) \
-  asm volatile("mov " SP ", %0": "=g"(sp)); 
-#define PULL(newsp) \
-  asm volatile("mov %0, " SP : "+g"(newsp));
-#define PU(sp,newsp)\
-  asm volatile("mov " SP ", %0": "=g"(sp)); \
-  asm volatile("mov %0, " SP : "+g"(newsp));
 
-static int id=1;
+static int id=0;
 enum co_status {
   CO_NEW = 1, // 新创建，还未执行过
   CO_RUNNING, // 已经执行过
@@ -76,7 +64,7 @@ void wrapper(struct co* co) {
 
 __attribute__((constructor)) void co_init() {
   co_main=malloc(sizeof(struct co));
-  co_main->id=0;
+  co_main->id=id++;
   co_main->name="main";
   co_main->status=CO_RUNNING;
   co_main->arg=NULL;
@@ -131,7 +119,6 @@ void co_yield(){
   int val=setjmp(co_current->context);
   if(val==0){
     int r=rand()%(id);
-    struct co *prev=co_current;
     co_current=coroutines;
     while(r>0 || co_current->status==CO_DEAD || co_current->status==CO_WAITING){
       co_current=co_current->next;
@@ -142,7 +129,7 @@ void co_yield(){
       stack_switch_call(co_current->stackptr,wrapper,(uintptr_t)co_current);
     }
     else{
-      Log("longjmp to %d %s from yield",co_current->id,co_current->name);
+      Log("longjmp to %s %d from yield",co_current->name,co_current->id);
       longjmp(co_current->context,1); 
     }
   }
@@ -157,143 +144,6 @@ void co_wait(struct co *co){
   co->waiter=co_current;
   co_current->status=CO_WAITING;
   co_yield();
-  co_delete(co);
+  co_wait(co);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*__attribute__((constructor)) void co_init() {
-  co_main=malloc(sizeof(struct co));
-  co_main->id=0;
-  co_main->name="main";
-  co_main->status=CO_RUNNING;
-  co_main->arg=NULL;
-  memset(co_main->stack,0,sizeof(co_main->stack));
-  co_main->stackptr=co_main->stack+sizeof(co_main->stack);
-  co_main->next=NULL;
-  co_main->prev=NULL;
-  co_current=co_main;
-}
-
-struct co *co_generate(const char *name, void (*func)(void *), void *arg){
-  struct co *new_co=malloc(sizeof(struct co));
-  new_co->status=CO_NEW;
-  new_co->id=id++;
-  memset(new_co->stack,0,sizeof(new_co->stack));
-  new_co->stackptr=new_co->stack+sizeof(new_co->stack);
-  new_co->func=func;
-  new_co->arg=arg;
-  if(coroutines==NULL){
-    coroutines=new_co;
-    coroutines->next=coroutines;
-    coroutines->prev=coroutines;
-  }
-  else{
-    struct co* prev=coroutines->prev;
-    struct co* next=coroutines;
-    new_co->next=next;
-    next->prev=new_co;
-    new_co->prev=prev;
-    prev->next=new_co;
-  }
-  return new_co; 
-}
-struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-  Log("create co %d,%s",id,name);
-  return co_generate(name,func,arg);
-}
-void co_delete(struct co *thd){
-  if (coroutines==NULL) return;
-  if(coroutines->next==coroutines && thd==coroutines){
-    free(coroutines);
-    coroutines=NULL;
-    id--;
-    return;
-  }
-  struct co *next=thd->next;
-  struct co *prev=thd->prev;
-  if(coroutines==thd) coroutines=next;
-  free(thd);
-  id--;
-  next->prev=prev;
-  prev->next=next;
-};
-void co_wait(struct co *co) {
-  Log("start wait");
-  int val=setjmp(co_current->context);
-  Log("cur %d start wait for thd %d,val:%d",co_current->id,co->id,val);
-  if(val==0){
-      co_current=co;
-      if(co_current->status==CO_NEW){
-        co_current->status=CO_RUNNING;
-        Log("a new co %d start to run",co_current->id);
-        PU(co_main->stackptr,co_current->stackptr);
-        co_current->func(co_current->arg);
-        co_current->status=CO_DEAD;
-        longjmp(co_main->context,1);
-      }
-      else{
-        Log("long jmp to %d from wait",co_current->id);
-        longjmp(co_current->context,1);
-      }
-  }
-  Log("cur %d,co %d,delete",co_current->id,co->id);
-  co_current=co_main;
-  co_delete(co);
-}
-
-void co_yield(){
-  if(coroutines==NULL) return;
-  int val=setjmp(co_current->context);
-  if(val==0){
-    if(co_current==co_main){
-      if(coroutines->status==CO_NEW){
-        coroutines->status=CO_RUNNING;
-        Log("a new co %d start to run",coroutines->id);
-        PU(co_current->stackptr,coroutines->stackptr);
-        co_current=coroutines;
-        co_current->func(co_current->arg);
-        co_current->status=CO_DEAD;
-        longjmp(co_main->context,1);
-      }
-      else{
-        co_current=coroutines;
-        Log("longjmp to %d from yield",co_current->id);
-        longjmp(co_current->context,1);
-      }
-    }
-    else{
-      if(co_current->next->status==CO_NEW){
-        co_current->next->status=CO_RUNNING;
-        Log("a new co %d start to run",co_current->next->id);
-        PU(co_current->stackptr,co_current->next->stackptr);
-        Log("%d,%d",co_current->id,co_current->next->id);
-        co_current=co_current->next;
-        co_current->func(co_current->arg);
-        co_current->status=CO_DEAD;
-        longjmp(co_main->context,1);
-      }
-      else{
-        co_current=co_current->next;
-        Log("longjmp to %d from yield",co_current->id);
-        longjmp(co_current->context,1);
-      }
-    }
-  }
-  Log("yield finish");
-}*/
 
