3e2ae82d77ec6cfbdb87f03aeb3215d7b692c11b
tracer-nju <tracer@nju.edu.cn>
2020-03-18 19:41:58 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 4cd3c83032514a8a8dc3ee2e45a9590b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  19:41:58 up  1:37,  1 user,  load average: 0.50, 0.57, 0.54

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -24,23 +24,22 @@ static void pmm_init() {
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, _heap.start, _heap.end);
   head=(page_t *)_heap.start;
   page_t *cp=head;
-  list->next=&head->list;
-  head->list.prev=list;
+  list->next=head->list;
+  head->list->prev=list;
   for(int i=0;i<10;i++){
     cp->slab_size=rand()%32;
     cp->obj_cnt=rand()%3;
     cp->addr=(void*)cp;
     page_t *tmp=cp;
-    printf("%p,\n",&cp->list);
     cp++;
-    tmp->list.next=&cp->list;
-    cp->list.prev=&tmp->list;
+    tmp->list->next=cp->list;
+    cp->list->prev=tmp->list;
   }
   page_t *p=head;
   for(int i=0;i<10;i++){    
     //p本身指向page的首地址，p->addr也是；p->list是page中member list的首地址，p->list.prev指向上一个page的list的首地址
-    printf("%d,%d,%p,%p,%p,%p,%p\n",p->slab_size,p->obj_cnt,p,p->addr,&p->list,p->list.prev,p->list.next);
-    page_t *task=list_entry(&p->list,page_t,list);
+    printf("%d,%d,%p,%p,%p,%p,%p\n",p->slab_size,p->obj_cnt,p,p->addr,p->list,p->list->prev,p->list->next);
+    page_t *task=list_entry(p->list,page_t,list);
     printf("%p\n",task);
     p++;
   }
