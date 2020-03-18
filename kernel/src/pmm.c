2f7191c217b4a98538acdf17f56b621d47907aea
tracer-nju <tracer@nju.edu.cn>
2020-03-18 19:37:54 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 4cd3c83032514a8a8dc3ee2e45a9590b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  19:37:54 up  1:33,  1 user,  load average: 0.72, 0.51, 0.50

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -11,6 +11,14 @@ static page_t *head=NULL;
 static list_head list_h;
 static list_head *list=&list_h;
 
+#define list_entry(ptr, type, member) \
+  ((type *) \
+    ( (char *)(ptr) - (uintptr_t)(&((type *)0)->member) ) \
+  )
+
+// ptr to struct list_head
+//struct task *task = list_entry(ptr, struct task, wait_queue);
+
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)_heap.end - (uintptr_t)_heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, _heap.start, _heap.end);
@@ -29,8 +37,11 @@ static void pmm_init() {
     cp->list.prev=&tmp->list;
   }
   page_t *p=head;
-  for(int i=0;i<10;i++){
+  for(int i=0;i<10;i++){    
+    //p本身指向page的首地址，p->addr也是；p->list是page中member list的首地址，p->list.prev指向上一个page的list的首地址
     printf("%d,%d,%p,%p,%p,%p,%p\n",p->slab_size,p->obj_cnt,p,p->addr,&p->list,p->list.prev,p->list.next);
+    page_t *task=list_entry(&p->list,page_t,list);
+    printf("%p\n",task);
     p++;
   }
   panic("ddd");
