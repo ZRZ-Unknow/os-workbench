8553b999737481a50fb2fb5bedf35583e916b882
tracer-nju <tracer@nju.edu.cn>
2020-03-18 19:25:01 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 4cd3c83032514a8a8dc3ee2e45a9590b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  19:25:01 up  1:21,  1 user,  load average: 0.75, 0.69, 0.64

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,16 +1,5 @@
 #include <common.h>
 
-typedef union page {
-  struct {
-    spinlock_t lock; // 锁，用于串行化分配和并发的 free
-    int obj_cnt;     // 页面中已分配的对象数，减少到 0 时回收页面
-    int slab_size;
-    void *addr;      //首地址
-    //list_head list;  // 属于同一个线程的页面的链表
-    union page *next;
-  }; // 匿名结构体
-  uint8_t header[HDR_SIZE], data[PAGE_SIZE - HDR_SIZE];
-} __attribute__((packed)) page_t;  //告诉编译器取消结构在编译过程中的优化对齐,按照实际占用字节数进行对齐
 
 static void *kalloc(size_t size) {
   return NULL;
@@ -19,20 +8,25 @@ static void *kalloc(size_t size) {
 static void kfree(void *ptr) {
 }
 static page_t *head=NULL;
+static list_head list_h;
+static list_head *list=&list_h;
+
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)_heap.end - (uintptr_t)_heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, _heap.start, _heap.end);
   head=(page_t *)_heap.start;
   page_t *cp=head;
+  list->next=&head->list;
+  head->list.prev=list;
   for(int i=0;i<10;i++){
     cp->slab_size=rand()%32;
     cp->obj_cnt=rand()%3;
     cp->addr=(void*)cp;
-    cp++;
+    cp+=1;
   }
   page_t *p=head;
   for(int i=0;i<10;i++){
-    printf("%d,%d,%p,%p,%p\n",p->slab_size,p->obj_cnt,p,p->addr,p->next);
+    printf("%d,%d,%p,%p,%p\n",p->slab_size,p->obj_cnt,p,p->addr,p->list.next);
     p++;
   }
   panic("ddd");
