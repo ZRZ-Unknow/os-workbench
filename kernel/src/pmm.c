49e64954fd4dee16977a6350851079a92de7c84c
tracer-nju <tracer@nju.edu.cn>
2020-03-19 13:03:00 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: bd3024d03aa3432393f435239545474f   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  13:03:00 up  3:40,  1 user,  load average: 1.14, 0.78, 0.55

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -128,9 +128,9 @@ static void pmm_init() {
     kmc[i].partial_slab.prev=NULL;
     page_t *new_page=page_init(5);
     kmc[i].free_slab.next=&new_page->list;
-    debug_slab_print(new_page);
+    //debug_slab_print(new_page);
   }
-  //debug_print();
+  debug_print();
     //p本身指向page的首地址，p->addr也是；p->list是page中member list的首地址，p->list.prev指向上一个page的list的首地址
     //printf("%d,%d,%p,%p,%p,%p,%p\n",p->slab_size,p->obj_cnt,p,p->addr,&p->list,p->list.prev,p->list.next);
     //page_t *task=list_entry(&p->list,page_t,list);
