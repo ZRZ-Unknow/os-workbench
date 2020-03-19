1705cd5993c28e3807b44744f7a26dd7e059050f
tracer-nju <tracer@nju.edu.cn>
2020-03-19 23:17:54 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: aa84e610ee2e4a958e02bbb8c918e272   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  23:17:54 up  5:02,  1 user,  load average: 0.44, 0.62, 0.55

--- kernel/src/spinlock.c
+++ kernel/src/spinlock.c
@@ -13,7 +13,7 @@ void lock_acquire(spinlock_t *lk){
     #ifdef INTERRUPT   //当无中断时在native执行cli,sti指令会segmentation fault
     pushcli();
     #endif
-    if(holding(lk)) Spanic("acquire");
+    if(holding(lk)) Spanic("acquire,name:%s",lk->name);
     while(_atomic_xchg((intptr_t*)&lk->locked,1)!=0);
     __sync_synchronize();
     lk->cpu=_cpu();
@@ -21,7 +21,7 @@ void lock_acquire(spinlock_t *lk){
 }
 
 void lock_release(spinlock_t *lk){
-    if(!holding(lk)) Spanic("release");
+    if(!holding(lk)) Spanic("release,name:%s",lk->name);
     lk->cpu=-1;
     __sync_synchronize();
     _atomic_xchg((intptr_t*)&lk->locked,0);
