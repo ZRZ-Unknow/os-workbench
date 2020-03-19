18fa1f128459f04b565d0039be566cd1a0753a25
tracer-nju <tracer@nju.edu.cn>
2020-03-19 14:15:38 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: bd3024d03aa3432393f435239545474f   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  14:15:39 up  4:52,  1 user,  load average: 0.83, 0.78, 0.88

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -14,9 +14,9 @@ void *get_free_obj(page_t* page){
   void *ret=NULL;
   for(;pos<page->obj_num;pos++){
     if(page->bitmap[pos]==0){
-      //lock_acquire(&lk);
+      lock_acquire(&lk);
       Log("find free pos:%d\n",pos);
-      //lock_release(&lk);
+      lock_release(&lk);
       page->bitmap[pos]=1;
       int offset=pos*page->slab_size;
       ret=page->s_mem+offset;
@@ -110,9 +110,9 @@ static void pmm_init() {
 
 static void *kalloc(size_t size) {
   size=align_size(size);
-  //lock_acquire(&lk);
+  lock_acquire(&lk);
   Log("start alloc size %d\n",size); 
-  //lock_release(&lk);
+  lock_release(&lk);
   int cpu=_cpu();
   void *ret=NULL;
   if(kmc[cpu].free_slab.next!=NULL){
@@ -135,7 +135,7 @@ static void *kalloc(size_t size) {
   else{
     TODO();
   }
-  assert( !(((intptr_t)ret)%size)); 
+  assert( !(((intptr_t)ret)&size)); 
   return ret;
 }
 
