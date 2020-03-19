d3a3e11156e29aed8eee027f75bc0459721b3221
tracer-nju <tracer@nju.edu.cn>
2020-03-19 19:20:40 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: aa84e610ee2e4a958e02bbb8c918e272   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  19:20:41 up  1:04,  1 user,  load average: 0.77, 0.64, 0.44

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -10,15 +10,15 @@ static void os_run() {   //可以随意改动
   //lock_acquire(&lk);
   //lock_release(&lk);
   while(1){
-    printf("dddddddd,%d\n",_cpu());
-    /*for(int i=0;i<10;i++){
-      size_t size=rand()%128;
+    //printf("dddddddd,%d\n",_cpu());
+    for(int i=0;i<10;i++){
+      size_t size=8;//rand()%128;
       void *ret=pmm->alloc(size);
       while(!ret);
       lock_acquire(&lk);
       printf("cpu %d alloc [%p,%p),size:%d.\n",_cpu(),ret,ret+size,size);
       lock_release(&lk);
-    }*/
+    }
   }
   printf("ddd\n");
   while(1);
