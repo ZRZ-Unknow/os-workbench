52665b4fe299a3f63dc2f54f6d8bf16d00e33008
tracer-nju <tracer@nju.edu.cn>
2020-03-22 22:09:11 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: ace22ffe32744db68254e338b3650773   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  22:09:12 up  5:32,  1 user,  load average: 0.82, 0.65, 0.63

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -149,6 +149,7 @@ static void kfree(void *ptr) {
   Assert(page->bitmap[pos]==1,"ptr:[%p,%p),size:%d",ptr,ptr+page->slab_size,page->slab_size);
   page->bitmap[pos]=0;
   memset(ptr,0,page->slab_size);
+  assert(0);
   //if(page->obj_cnt==0){
     int n=get_slab_pos(page->slab_size);
     list_head *lh=page->list.prev;
