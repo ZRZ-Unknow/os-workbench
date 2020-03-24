c25ea71381ce115ec6c15375710937af8d210c09
tracer-nju <tracer@nju.edu.cn>
2020-03-24 18:22:05 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: e75d3cb7345a4f75a2432bd967604bb1   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  18:22:05 up  1:55,  1 user,  load average: 0.98, 0.60, 0.52

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -109,7 +109,7 @@ page_t *get_free_page(int num,int slab_size,int cpu){
 }
 void heap_init(){
   page_t *p=mem_start;
-  page_t *prev=mem_start;
+  //page_t *prev=mem_start;
   heap_free_mem.freepage_list.prev=NULL;
   heap_free_mem.freepage_list.next=&p->list;
   p->list.prev=&heap_free_mem.freepage_list;
@@ -120,7 +120,7 @@ void heap_init(){
     prev++;
     p++;
   }*/
-  prev->list.next->next=NULL;
+  //prev->list.next->next=NULL;
   /*page_t *pp=list_entry(heap_free_mem.freepage_list.next,page_t,list);
   while(1){
     printf("%p\n",(void*)pp);
