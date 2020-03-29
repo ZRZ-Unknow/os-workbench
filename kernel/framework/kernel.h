611514fb81de11020baa05c82e7443791bef60ab
Yanyan Jiang <jiangyy@outlook.com>
2020-03-29 21:52:32 +0800

    L2 - kmt

--- kernel/framework/kernel.h
+++ kernel/framework/kernel.h
@@ -10,13 +10,31 @@
   mod_##mod##_t *mod = &__##mod##_obj; \
   mod_##mod##_t __##mod##_obj
 
+typedef _Context *(*handler_t)(_Event, _Context *);
 MODULE(os) {
   void (*init)();
   void (*run)();
+  _Context *(*trap)(_Event ev, _Context *context);
+  void (*on_irq)(int seq, int event, handler_t handler);
 };
 
 MODULE(pmm) {
   void  (*init)();
   void *(*alloc)(size_t size);
   void  (*free)(void *ptr);
+};
+
+typedef struct task task_t;
+typedef struct spinlock spinlock_t;
+typedef struct semaphore sem_t;
+MODULE(kmt) {
+  void (*init)();
+  int  (*create)(task_t *task, const char *name, void (*entry)(void *arg), void *arg);
+  void (*teardown)(task_t *task);
+  void (*spin_init)(spinlock_t *lk, const char *name);
+  void (*spin_lock)(spinlock_t *lk);
+  void (*spin_unlock)(spinlock_t *lk);
+  void (*sem_init)(sem_t *sem, const char *name, int value);
+  void (*sem_wait)(sem_t *sem);
+  void (*sem_signal)(sem_t *sem);
 };
