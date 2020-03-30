feb273a808b9a8441586e5fa4f9e9f598df31d4f
tracer-nju <tracer@nju.edu.cn>
2020-03-30 18:29:38 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: a321300c7aef40d58132b7b9b5e7844a   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  18:29:38 up 26 min,  1 user,  load average: 0.39, 0.45, 0.46

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -5,20 +5,21 @@
 int main(int argc, char *argv[]) {
 
 
-  char *exec_argv[argc+2]; //= { "strace", "-T"};
+  char *exec_argv[argc+2];
   for(int i=0;i<argc+2;i++){
     if(i==0) exec_argv[i]="strace";
     else if(i==1) exec_argv[i]="-T";
     else if(i==argc+1) exec_argv[i]=NULL;
     else exec_argv[i]=argv[i-1];
   }
-  for(int i=0;i<argc+2;i++)
-    printf("%s ",exec_argv[i]);
   char *exec_envp[] = { "PATH=/bin", NULL, };
-  //execve("strace",          exec_argv, exec_envp);
-  //execve("/bin/strace",     exec_argv, exec_envp);
-  execve("/usr/bin/strace", exec_argv, exec_envp);
-
-  perror(argv[0]);
-  exit(EXIT_FAILURE);
+  //execve("/usr/bin/strace", exec_argv, exec_envp);
+  int pid=fork();
+  if(pid==0){
+    execve("/usr/bin/strace", exec_argv, exec_envp);
+  }
+  else{
+    printf("ddd\n");
+  }
+  return 0;
 }
