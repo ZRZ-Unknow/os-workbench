54f70660816864371b66794067c7e71a5c649853
tracer-nju <tracer@nju.edu.cn>
2020-03-30 18:12:29 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: a321300c7aef40d58132b7b9b5e7844a   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  18:12:29 up 9 min,  1 user,  load average: 0.20, 0.50, 0.36

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,11 +3,13 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  char *exec_argv[] = { "strace", "ls", NULL, };
+
+
+  char *exec_argv[] = { "strace", "-T","ls", NULL, };
   char *exec_envp[] = { "PATH=/bin", NULL, };
-  execve("strace",          exec_argv, exec_envp);
-  execve("/bin/strace",     exec_argv, exec_envp);
-  //execve("/usr/bin/strace", exec_argv, exec_envp);
+  //execve("strace",          exec_argv, exec_envp);
+  //execve("/bin/strace",     exec_argv, exec_envp);
+  execve("/usr/bin/strace", exec_argv, exec_envp);
   perror(argv[0]);
   exit(EXIT_FAILURE);
 }
