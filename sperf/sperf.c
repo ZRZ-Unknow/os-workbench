9daf5b5d122a2e0e369359fa9d59d4aac1a82d47
tracer-nju <tracer@nju.edu.cn>
2020-04-01 11:19:05 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 9f1701c59451450db9b6a2ab789b46e5   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  11:19:05 up  1:49,  1 user,  load average: 0.50, 0.76, 0.84

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -114,16 +114,11 @@ int main(int argc, char *argv[]) {
   char cmd_path[256];
   char *c_path;
   if(strncmp("/",cmd,1)!=0){
-    printf("%s--\n",cmd);
     c_path=find_path(cmd);
     sprintf(cmd_path,"%s/%s",c_path,cmd);
-    printf("--%s\n",cmd_path);
   }
   else sprintf(cmd_path,"%s",argv[1]);
   exec_argv[2]=&cmd_path[0];
-
-  printf("%s\n",exec_argv[2]);
-  assert(0);
 
   int fildes[2];
   if(pipe(fildes)!=0) assert(0);
