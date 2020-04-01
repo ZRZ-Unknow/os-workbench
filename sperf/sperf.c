9bb7c9dbd34a944fd661e6fb995994f8559ba1a2
tracer-nju <tracer@nju.edu.cn>
2020-04-01 10:48:03 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 9f1701c59451450db9b6a2ab789b46e5   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  10:48:04 up  1:18,  1 user,  load average: 2.33, 1.44, 1.03

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -146,7 +146,7 @@ int main(int argc, char *argv[]) {
         continue;
       }
       end=time(NULL);
-      if((double)(end-begin)>0.1){
+      if((double)(end-begin)>1){
         sort();
         display();
         begin=time(NULL);
