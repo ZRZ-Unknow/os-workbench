3137091c71b06ec52c16406ae7040be4eb70519c
tracer-nju <tracer@nju.edu.cn>
2020-04-01 16:49:40 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 9f1701c59451450db9b6a2ab789b46e5   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  16:49:40 up  7:19,  1 user,  load average: 0.71, 0.56, 0.66

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -106,7 +106,7 @@ int main(int argc, char *argv[]) {
   char exec_path[64];
   
   char *path=find_path("strace");
-  assert(path);
+  assert(path!=NULL);
   printf("path is %s\n",find_path("strace"));
   //sprintf(envp_path,"PATH=%s",path);
   sprintf(exec_path,"%s/%s",path,"strace");
