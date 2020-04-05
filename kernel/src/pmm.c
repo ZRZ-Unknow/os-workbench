5e74a330986e4e16ee87feed33c50e7727262bb5
tracer-nju <tracer@nju.edu.cn>
2020-04-05 22:48:44 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: f5d1c5a6c27a49d09d04ef2d8d8d6b51   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  22:48:44 up  2:40,  1 user,  load average: 0.56, 0.37, 0.51

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -25,7 +25,7 @@ int get_slab_pos(int size){
 //调用前先上锁
 void *get_free_obj(page_t* page){
   void *ret=NULL;
-  int bitmap_num= page->obj_num/32+1;
+  int bitmap_num= (page->obj_num%32==0)?(page->obj_num/32):(page->obj_num/32+1);  //page->obj_num/32+1;
   for(int i=0;i<bitmap_num;i++){
     if(page->bitmap[i]==I) continue;
     int pos;
