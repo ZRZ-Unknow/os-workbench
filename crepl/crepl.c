99f0274ebbcefe44c8459d0ea431aa2c5744c41d
tracer-nju <tracer@nju.edu.cn>
2020-04-07 09:55:42 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: ea051a1a7e6740dca64676ab333e3863   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  09:55:42 up  1:26,  1 user,  load average: 1.09, 0.58, 0.45

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+static char line[4096];
+static char func_name[128];
+static char wrap_name[128];
+static int func_num=0;
+static int wrap_num=0;
+
+void compile(){
+}
+
 int main(int argc, char *argv[]) {
-  static char line[4096];
-  char tmp[4];
+  static char tmp[4];
   while (1) {
     printf("crepl> ");
     memset(line,'\0',sizeof(line));
@@ -14,12 +22,10 @@ int main(int argc, char *argv[]) {
     }
     sscanf(line,"%3s",tmp);
     if(strncmp(line,"int",3)==0){
-      printf("It's a function\n");
+      compile();
     }
     else{
       printf("It's a expression\n");
     }
-    printf("%s",line);
-    printf("%s\n",tmp);
   }
 }
