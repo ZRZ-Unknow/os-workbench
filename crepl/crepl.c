ea1b925ed548f5718b49e0be1315ba7e38c6c70c
tracer-nju <tracer@nju.edu.cn>
2020-04-07 09:31:30 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: ea051a1a7e6740dca64676ab333e3863   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  09:31:29 up  1:01,  1 user,  load average: 0.43, 0.48, 0.56

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -3,16 +3,16 @@
 
 int main(int argc, char *argv[]) {
   static char line[4096];
-  //char tmp[4];
+  char tmp[4];
   while (1) {
     printf("crepl> ");
     memset(line,'\0',sizeof(line));
-   // memset(tmp,'\0',sizeof(tmp));
+    memset(tmp,'\0',sizeof(tmp));
     fflush(stdout);
     if (!fgets(line, sizeof(line), stdin)) {
       break;
     }
-    //sscanf(line,"%3s",tmp);
+    sscanf(line,"%3s",tmp);
     if(strncmp(line,"int",3)==0){
       printf("It's a function\n");
     }
@@ -20,6 +20,6 @@ int main(int argc, char *argv[]) {
       printf("It's a expression\n");
     }
     printf("%s",line);
-    //printf("%s\n",tmp);
+    printf("%s\n",tmp);
   }
 }
