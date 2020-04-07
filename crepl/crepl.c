686390184bccf6e637a6a70cea7c94da1a44ac6b
tracer-nju <tracer@nju.edu.cn>
2020-04-07 13:38:14 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: ea051a1a7e6740dca64676ab333e3863   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  13:38:15 up  5:08,  1 user,  load average: 0.97, 0.54, 0.47

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -32,8 +32,6 @@ void compile(bool func){
   else fprintf(fp,"int wrap_func(){return (%s);}",line);
   fclose(fp);
   char *exec_argv[]={"gcc",TARGET,"-x","c","-fPIC","-w","-shared","-o",dst_filename,src_filename,NULL};
-  //int fildes[2];
-  //if(pipe(fildes)!=0) printf("\033[1;31m      Pipe Failed!\033[0m\n");
   int pid=fork();
   if(pid==0){
     int fd=open("/dev/null",O_RDWR);
@@ -42,8 +40,6 @@ void compile(bool func){
     execvp(exec_argv[0],exec_argv);
   }
   else{
-/*wait(int *status):父进程一旦调用了wait就立即阻塞自己，由wait自动分析是否当前进程的某个子进程已经退出，如果让它找到了这样一个已经变成僵尸的子进程，
-  wait就会收集这个子进程的信息，并把它彻底销毁后返回；如果没有找到这样一个子进程，wait就会一直阻塞在这里，直到有一个出现为止。*/
     int status;
     wait(&status);
     if(status!=0) printf("\033[1;31m      Compile Error!\033[0m\n");
