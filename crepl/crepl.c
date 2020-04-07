1251e40d4402c8d824528f00cb755eeaa62b517b
tracer-nju <tracer@nju.edu.cn>
2020-04-07 09:22:55 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: ea051a1a7e6740dca64676ab333e3863   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  09:22:56 up 53 min,  1 user,  load average: 0.67, 0.56, 0.65

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -5,9 +5,16 @@ int main(int argc, char *argv[]) {
   static char line[4096];
   while (1) {
     printf("crepl> ");
+    memset(line,'\0',sizeof(line));
     fflush(stdout);
     if (!fgets(line, sizeof(line), stdin)) {
       break;
+    }
+    if(strncmp(line,"int",3)==0){
+      printf("It's a function\n");
+    }
+    else{
+      printf("It's a expression\n");
     }
     for(int i=0;i<strlen(line);i++){
       printf("%d-",line[i]);
