dcd90894a9206ae2855d78f38eda9739e1948ca5
tracer-nju <tracer@nju.edu.cn>
2020-04-07 13:34:56 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: ea051a1a7e6740dca64676ab333e3863   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  13:34:56 up  5:05,  1 user,  load average: 0.77, 0.59, 0.49

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -19,20 +19,21 @@ static char line[4096];
 static char tmp[4];
 static char src_filename[32];
 static char dst_filename[32];
-int (*f)();
+static int (*f)();
+static int value;
 
 void compile(bool func){
   sprintf(src_filename,"/tmp/func_c_XXXXXX");
   sprintf(dst_filename,"/tmp/func_so_XXXXXX");
-  if(mkstemp(src_filename)==-1) printf("mkstemp failed\n");
-  if(mkstemp(dst_filename)==-1) printf("mkstemp failed\n");
+  if(mkstemp(src_filename)==-1) printf("\033[1;31m      Mkstemp Failed!\033[0m\n");
+  if(mkstemp(dst_filename)==-1) printf("\033[1;31m      Mkstemp Failed!\033[0m\n");
   FILE *fp=fopen(src_filename,"w");
   if(func) fprintf(fp,"%s",line);
-  else fprintf(fp,"int wrap_func(){return %s;}",line);
+  else fprintf(fp,"int wrap_func(){return (%s);}",line);
   fclose(fp);
   char *exec_argv[]={"gcc",TARGET,"-x","c","-fPIC","-w","-shared","-o",dst_filename,src_filename,NULL};
-  int fildes[2];
-  if(pipe(fildes)!=0) assert(0);
+  //int fildes[2];
+  //if(pipe(fildes)!=0) printf("\033[1;31m      Pipe Failed!\033[0m\n");
   int pid=fork();
   if(pid==0){
     int fd=open("/dev/null",O_RDWR);
@@ -47,61 +48,23 @@ void compile(bool func){
     wait(&status);
     if(status!=0) printf("\033[1;31m      Compile Error!\033[0m\n");
     else{
-        void *handle=dlopen(dst_filename,RTLD_NOW|RTLD_GLOBAL);
-        unlink(src_filename);
-        unlink(dst_filename);
-        if(!handle) printf("\033[1;31m      Compile Error!\033[0m\n");
-        else{ 
-          if(func) printf("\033[1;32m      Added: \033[1;30m%s\033[0m",line);
-          else{
-            f=dlsym(handle,"wrap_func");
-            int value=f();
-            printf("\033[1;32m      Result: \033[1;30m%d\033[0m\n",value);
-            dlclose(handle); 
-          }
-        }
-      }
-  }
-}
-void run(){
-  sprintf(src_filename,"/tmp/func_c_XXXXXX");
-  sprintf(dst_filename,"/tmp/func_so_XXXXXX");
-  if(mkstemp(src_filename)==-1) printf("mkstemp failed\n");
-  if(mkstemp(dst_filename)==-1) printf("mkstemp failed\n");
-  FILE *fp=fopen(src_filename,"w");
-  fprintf(fp,"int wrap_func(){return %s;}",line);
-  fclose(fp);
-  char *exec_argv[]={"gcc",TARGET,"-x","c","-fPIC","-w","-shared","-o",dst_filename,src_filename,NULL};
-  int fildes[2];
-  if(pipe(fildes)!=0) assert(0);
-  int pid=fork();
-  if(pid==0){
-    int fd=open("/dev/null",O_RDWR);
-    dup2(fd,STDOUT_FILENO);
-    dup2(fd,STDERR_FILENO);
-    execvp(exec_argv[0],exec_argv);
-  }
-  else{
-    int status;
-    wait(&status);
-    if(WIFEXITED(status)!=0){
-      if(WEXITSTATUS(status)!=0) printf("\033[1;31m      Compile Error!\033[0m\n");
-      else{
-        void *handle=dlopen(dst_filename,RTLD_LAZY|RTLD_GLOBAL);
-        if(!handle) printf("\033[1;31m      Compile Error!\033[0m\n");
+      void *handle=dlopen(dst_filename,RTLD_NOW|RTLD_GLOBAL);
+      unlink(src_filename);
+      unlink(dst_filename);
+      if(!handle) printf("\033[1;31m      Compile Error!\033[0m\n");
+      else{ 
+        if(func) printf("\033[1;32m      Added: \033[1;30m%s\033[0m",line);
         else{
           f=dlsym(handle,"wrap_func");
-          int value=f();
-          printf("\033[1;32m      Result: \033[1;30m%d\033[0m\n",value); 
-        } 
-        dlclose(handle);
+          value=f();
+          printf("\033[1;32m      Result: \033[1;30m%d\033[0m\n",value);
+          dlclose(handle); 
+        }
       }
     }
-    else printf("\033[1;31m      Compile Error!\033[0m\n");
   }
-  unlink(src_filename);
-  unlink(dst_filename);
 }
+
 int main(int argc, char *argv[]) {
   while (1) {
     printf("crepl> ");
@@ -112,25 +75,7 @@ int main(int argc, char *argv[]) {
       break;
     }
     sscanf(line,"%3s",tmp);
-    if(strncmp(line,"int",3)==0){
-      compile(true);
-    }
-    else{
-      compile(false);
-    }
+    compile(strncmp(line,"int",3)==0);
   }
 }
-/*void *dlopen( const char * pathname, int mode );
-以指定模式打开指定的动态链接库,返回一个句柄(handle),失败返回NULL
-  1、解析方式
-    RTLD_LAZY：在dlopen返回前，对于动态库中的未定义的符号不执行解析（只对函数引用有效，对于变量引用总是立即解析）。
-    RTLD_NOW： 需要在dlopen返回前，解析出所有未定义符号，如果解析不出来，在dlopen会返回NULL，错误为：: undefined symbol: xxxx.......
-  2、作用范围，可与解析方式通过“|”组合使用。 
-    RTLD_GLOBAL：动态库中定义的符号可被其后打开的其它库重定位。 
-    RTLD_LOCAL： 与RTLD_GLOBAL作用相反，动态库中定义的符号不能被其后打开的其它库重定位。如果没有指明是RTLD_GLOBAL还是RTLD_LOCAL，则缺省为RTLD_LOCAL
-  3、作用方式
-    RTLD_NODELETE： 在dlclose()期间不卸载库，并且在以后使用dlopen()重新加载库时不初始化库中的静态变量。这个flag不是POSIX-2001标准。 
-    RTLD_NOLOAD： 不加载库。可用于测试库是否已加载(dlopen()返回NULL说明未加载，否则说明已加载），也可用于改变已加载库的flag，如：先前加载库的flag为RTLD＿LOCAL，用dlopen(RTLD_NOLOAD|RTLD_GLOBAL)后flag将变成RTLD_GLOBAL。这个flag不是POSIX-2001标准。
-    RTLD_DEEPBIND：在搜索全局符号前先搜索库内的符号，避免同名符号的冲突。这个flag不是POSIX-2001标准。
-void *dlsym(void *handle, const char* symbol);
-handle是使用dlopen函数之后返回的句柄，symbol是要求获取的函数的名称，函数，返回值是void*,指向函数的地址，供调用使用*/
+
