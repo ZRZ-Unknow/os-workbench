21d0529c2b96ecd6e8b2ec1f4efdc184dde25620
tracer-nju <tracer@nju.edu.cn>
2020-04-07 13:43:12 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: ea051a1a7e6740dca64676ab333e3863   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  13:43:12 up  5:13,  1 user,  load average: 0.90, 0.56, 0.48

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -45,8 +45,6 @@ void compile(bool func){
     if(status!=0) printf("\033[1;31m      Compile Error!\033[0m\n");
     else{
       void *handle=dlopen(dst_filename,RTLD_NOW|RTLD_GLOBAL);
-      unlink(src_filename);
-      unlink(dst_filename);
       if(!handle) printf("\033[1;31m      Compile Error!\033[0m\n");
       else{ 
         if(func) printf("\033[1;32m      Added: \033[1;30m%s\033[0m",line);
@@ -59,6 +57,8 @@ void compile(bool func){
       }
     }
   }
+  unlink(src_filename);
+  unlink(dst_filename);
 }
 
 int main(int argc, char *argv[]) {
