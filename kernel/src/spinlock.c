2df786a5b7b71df0a7480db6e64e70dd04932f95
tracer-nju <tracer@nju.edu.cn>
2020-04-22 18:08:59 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 5f7d4b181c454ece9249be69aaa6a307   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  18:08:59 up 53 min,  1 user,  load average: 0.98, 1.00, 0.78

--- kernel/src/spinlock.c
+++ kernel/src/spinlock.c
@@ -1,7 +1,7 @@
 #include <common.h>
 
-//int ncli[MAX_CPU]={0};
-//int intena[MAX_CPU]={0};
+int ncli[MAX_CPU]={0};
+int intena[MAX_CPU]={0};
 
 void lock_init(spinlock_t *lk,char *name){
     lk->name=name;
@@ -10,22 +10,22 @@ void lock_init(spinlock_t *lk,char *name){
 }
 
 void lock_acquire(spinlock_t *lk){
-    //pushcli();
-    //if(holding(lk)) Spanic("acquire,name:%s",lk->name); //当lk为1且cpu为当前cpu时，即禁止重入
+    pushcli();
+    if(holding(lk)) Spanic("acquire,name:%s",lk->name); //当lk为1且cpu为当前cpu时，即禁止重入
     while(_atomic_xchg((intptr_t*)&lk->locked,1)!=0);
     __sync_synchronize();
     lk->cpu=_cpu();
-    //SLog("cpu %d acquire lk \"%s\"",lk->cpu,lk->name);
+    SLog("cpu %d acquire lk \"%s\"",lk->cpu,lk->name);
 }
 
 void lock_release(spinlock_t *lk){
-    //if(!holding(lk)) Spanic("release,name:%s",lk->name);  //当lk为0或者 lk的cpu不为当前cpu
+    if(!holding(lk)) Spanic("release,name:%s",lk->name);  //当lk为0或者 lk的cpu不为当前cpu
     lk->cpu=-1;
     __sync_synchronize();
     _atomic_xchg((intptr_t*)&lk->locked,0);
-    //popcli();
+    popcli();
 }
-/*
+
 int holding(spinlock_t *lk){
     int r;
     pushcli();
@@ -44,4 +44,4 @@ void popcli(void){
     if(get_efl() & FL_IF) Spanic("popcli interruptible");
     if(--ncli[_cpu()] <0 ) Spanic("popcli");
     if(ncli[_cpu()]==0 && intena[_cpu()]) sti();
-}*/
+}
