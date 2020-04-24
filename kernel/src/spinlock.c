cab345852247bd67d7020a47027368d3392304c8
tracer-nju <tracer@nju.edu.cn>
2020-04-24 11:39:52 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 95ef83dd38244992acf519314f411892   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  11:39:53 up  1:02,  1 user,  load average: 0.76, 0.58, 0.53

--- kernel/src/spinlock.c
+++ kernel/src/spinlock.c
@@ -1,7 +1,8 @@
 #include <common.h>
 
-int ncli[MAX_CPU]={0};
-int intena[MAX_CPU]={0};
+static int ncli[MAX_CPU]={0};
+static int intena[MAX_CPU]={0};
+static int intr_flag=-1;
 
 void lock_init(spinlock_t *lk,const char *name){
     lk->name=name;
@@ -10,6 +11,8 @@ void lock_init(spinlock_t *lk,const char *name){
 }
 
 void lock_acquire(spinlock_t *lk){
+    intr_flag=_intr_read();
+    _intr_write(0);
     pushcli();
     if(holding(lk)) Spanic("acquire,name:%s",lk->name); //当lk为1且cpu为当前cpu时，即禁止重入
     while(_atomic_xchg((intptr_t*)&lk->locked,1)!=0);
@@ -23,6 +26,7 @@ void lock_release(spinlock_t *lk){
     __sync_synchronize();
     _atomic_xchg((intptr_t*)&lk->locked,0);
     popcli();
+    if(intr_flag) _intr_write(1);
 }
 
 int holding(spinlock_t *lk){
