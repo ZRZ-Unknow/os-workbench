872022a275757ac2540c5438429c83277490959b
tracer-nju <tracer@nju.edu.cn>
2020-04-24 11:50:56 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 95ef83dd38244992acf519314f411892   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  11:50:56 up  1:13,  1 user,  load average: 0.47, 0.54, 0.58

--- kernel/src/spinlock.c
+++ kernel/src/spinlock.c
@@ -2,7 +2,6 @@
 
 static int ncli[MAX_CPU]={0};
 static int intena[MAX_CPU]={0};
-static int intr_flag=-1;
 
 void lock_init(spinlock_t *lk,const char *name){
     lk->name=name;
@@ -11,8 +10,6 @@ void lock_init(spinlock_t *lk,const char *name){
 }
 
 void lock_acquire(spinlock_t *lk){
-    intr_flag=_intr_read();
-    _intr_write(0);
     pushcli();
     if(holding(lk)) Spanic("acquire,name:%s",lk->name); //当lk为1且cpu为当前cpu时，即禁止重入
     while(_atomic_xchg((intptr_t*)&lk->locked,1)!=0);
@@ -26,7 +23,6 @@ void lock_release(spinlock_t *lk){
     __sync_synchronize();
     _atomic_xchg((intptr_t*)&lk->locked,0);
     popcli();
-    if(intr_flag) _intr_write(1);
 }
 
 int holding(spinlock_t *lk){
@@ -38,7 +34,8 @@ int holding(spinlock_t *lk){
 }
 void pushcli(void){
     uint32_t eflags=get_efl();
-    cli();
+    //cli();
+    _intr_write(0);
     if(ncli[_cpu()]==0) intena[_cpu()]= eflags & FL_IF;
     ncli[_cpu()]+=1;
 }
@@ -46,5 +43,5 @@ void pushcli(void){
 void popcli(void){
     if(get_efl() & FL_IF) Spanic("popcli interruptible");
     if(--ncli[_cpu()] <0 ) Spanic("popcli");
-    if(ncli[_cpu()]==0 && intena[_cpu()]) sti();
+    if(ncli[_cpu()]==0 && intena[_cpu()]) _intr_write(1);
 }
