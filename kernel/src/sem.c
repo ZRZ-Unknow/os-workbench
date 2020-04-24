7bcd018007d276d354d306d7e56b659905b33564
tracer-nju <tracer@nju.edu.cn>
2020-04-24 14:32:03 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 95ef83dd38244992acf519314f411892   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  14:32:03 up  3:54,  1 user,  load average: 0.85, 0.74, 0.74

--- kernel/src/sem.c
+++ kernel/src/sem.c
@@ -1,5 +1,11 @@
 #include <common.h>
 
-void sem_init(sem_t *sem, const char *name, int value);
-void sem_wait(sem_t *sem);
-void sem_signal(sem_t *sem);
+void sem_init(sem_t *sem, const char *name, int value){
+    return;
+}
+void sem_wait(sem_t *sem){
+    return;
+}
+void sem_signal(sem_t *sem){
+    return;
+}
