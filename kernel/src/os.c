bf038932c040c88025e8bf0958a22568cbdb1759
tracer-nju <tracer@nju.edu.cn>
2020-04-26 10:59:21 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: ee94d8df74f64b06bd11d5a72a5c1d34   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  10:59:21 up  2:22,  1 user,  load average: 1.89, 2.11, 2.03

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -132,7 +132,7 @@ static void os_run() {   //可以随意改动
 /*类似与thread-os-mp.c中的on_interrupt，每次中断后，AM会保存现场，然后调用os_trap进行中断处理，os_trap
   返回后，AM会恢复现场*/
 static _Context *os_trap(_Event ev,_Context *context){
-  if(_cpu()>1) return context;
+  if(_cpu()>0) return context;
   _Context *next=NULL;
   //if(holding(&os_trap_lk)) return context;
   lock_acquire(&os_trap_lk);
