64c5e9114b2e91656cb91eaac5f9f814f810d08f
tracer-nju <tracer@nju.edu.cn>
2020-05-20 15:39:24 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 74050cf9025d412c812e06e4fb79acca   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  15:39:24 up 15:16,  1 user,  load average: 0.91, 0.68, 0.56

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -7,6 +7,44 @@
 #include <stdbool.h>
 
 char filename[128];
+struct fat_header {
+  uint8_t  BS_jmpBoot[3];
+  uint8_t  BS_OEMName[8];
+  uint16_t BPB_BytsPerSec;
+  uint8_t BPB_SecPerClus;
+  uint16_t BPB_RsvdSecCnt;
+  uint8_t BPB_NumFATs;
+  uint16_t BPB_RootEntCnt;  //in FAT32 must be 0
+  uint16_t BPB_TotSec16;    //int FAT32 must be 0
+  uint8_t BPB_Media;
+  uint16_t BPB_FATSz16;     //0
+  uint16_t BPB_SecPerTrk;
+  uint16_t BPB_NumHeads;
+  uint32_t BPB_HiddSec;
+  uint32_t BPB_TotSec32;
+  uint32_t BPB_FATSz32;
+  uint16_t BPB_ExtFlags;
+  uint16_t BPB_FSVer;
+  uint32_t BPB_RootClus;
+  uint16_t BPB_FSInfo;
+  uint16_t BPB_BkBootSec;
+  uint8_t BPB_Reserved[12];
+  uint8_t BS_DrvNum;
+  uint8_t BS_Reserved1;
+  uint8_t BS_BootSig;
+  uint32_t BS_VollD;
+  uint8_t BS_VolLab[11];
+  uint32_t BS_FilSysType[2];
+  uint8_t  padding[420];
+  uint16_t Signature_word;
+} __attribute__((packed));
+
+struct fat {
+  struct fat_header header;
+  uint8_t padding[PADDING_SIZE];
+  //struct fat_table fat[FAT_COPIES];
+  //struct cluster clusters[CLUSTER_SIZE];
+} __attribute__((packed)); 
 
 void recover(){
 
@@ -16,6 +54,7 @@ void recover(){
 int main(int argc, char *argv[]) {
   assert(argc>=3);
   assert(strcmp(argv[1],"frecov")==0);
+  assert(sizeof(struct fat_header)==512);
   sprintf(filename,"%s",argv[2]);
   printf("%s\n",filename);
   recover();
