0f53d14af27e0e91ab7990a8d5c8ef3fd8f033ca
tracer-nju <tracer@nju.edu.cn>
2020-05-20 23:59:08 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 74050cf9025d412c812e06e4fb79acca   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  23:59:08 up 23:35,  1 user,  load average: 0.54, 0.54, 0.63

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -89,7 +89,7 @@ void recover(){
   struct DIR *dir=data_begin;
   while((uintptr_t)dir<(uintptr_t)(fat_fs+buf.st_size)){
     printf("%p,%p\n",dir,fat_fs+buf.st_size);
-    if((dir->data[11])==1){   //长文件名
+    if((dir->data[11])==0b00111100){   //长文件名
       printf("dd\n");
     }/**
     else if(dir->data[8]==(uint8_t)'B' && dir->data[9]==(uint8_t)'M' && dir->data[10]==(uint8_t)'P'){
