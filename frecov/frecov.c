c48ccd49c5613e986deb3b7d506b5fb108429112
tracer-nju <tracer@nju.edu.cn>
2020-05-21 00:28:32 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 74050cf9025d412c812e06e4fb79acca   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  00:28:32 up 1 day, 5 min,  1 user,  load average: 0.74, 0.67, 0.62

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -86,7 +86,7 @@ void recover(){
   assert(fat_fs!=MAP_FAILED);
   struct fat_header *header=fat_fs;
   assert(header->Signature_word==0xaa55);
-  void *data_begin=(void*)(uintptr_t)((header->BPB_RsvdSecCnt+header->BPB_NumFATs*header->BPB_FATSz32+(header->BPB_RootClus-2)*header->BPB_SecPerClus)*header->BPB_BytsPerSec);
+  void *data_begin=(void*)(int)((header->BPB_RsvdSecCnt+header->BPB_NumFATs*header->BPB_FATSz32+(header->BPB_RootClus-2)*header->BPB_SecPerClus)*header->BPB_BytsPerSec);
   printf("start:%p,data_begin:%p,size:%ld\n",fat_fs,data_begin,buf.st_size);
   printf("%d\n",header->Signature_word);
   struct DIR *dir=data_begin;
