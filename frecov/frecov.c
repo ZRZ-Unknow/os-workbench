65345e5b90798ee2297f5d3825222306005c02d2
tracer-nju <tracer@nju.edu.cn>
2020-05-20 13:44:31 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 74050cf9025d412c812e06e4fb79acca   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  13:44:31 up 13:21,  1 user,  load average: 0.53, 0.47, 0.31

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -9,9 +9,11 @@
 
 
 int main(int argc, char *argv[]) {
-    char buf[128];
-    for(int i=0;i<argc;i++){
-        printf("%s\n",argv[i]);
-    }
-    return 0;
+  char buf[128];
+  assert(argc>=3);
+  assert(argv[1]=="frecov");
+  sprintf(buf,"%s",argv[2]);
+  printf("%s\n",buf);
+  
+  return 0;
 }
