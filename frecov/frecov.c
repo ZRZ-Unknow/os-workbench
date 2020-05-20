39cd743f712489d4c3761e37e2181a725b6c6ff2
tracer-nju <tracer@nju.edu.cn>
2020-05-21 00:17:26 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 74050cf9025d412c812e06e4fb79acca   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  00:17:27 up 23:54,  1 user,  load average: 1.17, 0.69, 0.59

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -74,7 +74,7 @@ struct fat_long_dir{
 struct DIR{
   //uint8_t data1;
   //uint8_t data2;
-  uint8_t data[16];
+  uint8_t data;
 }__attribute__((packed));
 
 void recover(){
@@ -90,7 +90,7 @@ void recover(){
   printf("start:%p,data_begin:%p,size:%ld\n",fat_fs,data_begin,buf.st_size);
   printf("%d\n",header->Signature_word);
   struct DIR *dir=data_begin;
-  assert((dir->data)[0]==0x34);
+  assert(dir->(data)==0x34);
   while((uintptr_t)dir<(uintptr_t)(fat_fs+buf.st_size)){
     printf("%p,%p\n",dir,fat_fs+buf.st_size);
     //if((dir->data[11])==0b00111100){   //长文件名
