e6579c605a7dba7c98846779429e9b4c8ab0eebf
tracer-nju <tracer@nju.edu.cn>
2020-05-21 21:12:46 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: e8617457824f4d98abafe978e22d65f7   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  21:12:46 up  4:11,  1 user,  load average: 1.00, 1.03, 0.86

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -126,7 +126,7 @@ void recover(){
             }
           }
           if(getbit(long_dir->LDIR_Ord,6)==1){  //长文件名的最后一个目录项
-            strcat(long_name_lenth,".bmp");
+            strcat(long_name_buf,".bmp");
             printf("long_name:%s\n",long_name_buf);
             break;
           }
