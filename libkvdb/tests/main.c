e48ec5d3f36cc8c12ec816209074ac3c11bff625
tracer-nju <tracer@nju.edu.cn>
2020-06-28 17:10:16 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 195279116741420da398fe0e13546316   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  17:10:17 up 23:12,  1 user,  load average: 0.74, 0.69, 0.63

--- libkvdb/tests/main.c
+++ libkvdb/tests/main.c
@@ -1,5 +1,17 @@
-#include "kvdb-test.h"
+#include <kvdb.h>
 
 int main(){
+  struct kvdb *db;
+  const char *key = "operating-systems";
+  char *value;
+
+  panic_on(!(db = kvdb_open("a.db")), "cannot open db"); // 打开数据库
+
+  kvdb_put(db, key, "three-easy-pieces"); // db[key] = "three-easy-pieces"
+  value = kvdb_get(db, key); // value = db[key];
+  kvdb_close(db); // 关闭数据库
+  printf("[%s]: [%s]\n", key, value);
+  free(value);
+  return 0;
   return 0;
 }
