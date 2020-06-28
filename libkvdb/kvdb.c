90f65fc667c7ce0e937504fef7bf912192a18307
tracer-nju <tracer@nju.edu.cn>
2020-06-28 11:39:22 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 195279116741420da398fe0e13546316   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  11:39:23 up 17:41,  1 user,  load average: 0.54, 0.76, 0.76

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
 #include <assert.h>
+#include <sys/file.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
+
+#define B *1
+#define KB B*1024
+#define MB KB*1024
+#define JSIZE (32 MB)
+
+//读写文件数据 (以及管理偏移量) 时使用 read, write 和 lseek，同步数据时使用 fsync。
 struct kvdb {
-  // your definition here
+  int fd;
+  int start;  
+  int size;
+  int committing;
 };
 
 struct kvdb *kvdb_open(const char *filename) {
+  int fd=open(filename,O_RDWR|O_CREAT,S_IRUSR|S_IXUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
+  struct stat buf;
+  if(stat(filename,buf)!=0) assert(0);
+  struct kvdb *db=malloc(sizeof(struct kvdb));
+  db->fd=fd;
+  if(buf.st_size==0){
+
+  }
+  else{
+    //recover
+  }
+  
   return NULL;
 }
 
