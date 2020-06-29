e6b3540d82c2cdf0ea23635e0499d2338f30a2cb
tracer-nju <tracer@nju.edu.cn>
2020-06-29 16:32:31 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 1adcefe45a584d20b08dd6a93cd04e6c   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  16:32:31 up 18:18,  1 user,  load average: 1.87, 1.69, 1.59

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -137,10 +137,10 @@ int main() {
     setbuf(stdout, NULL);  //取消了输出缓存
 
     printf("Test #1. Expect: (X|Y){0, 1, 2, ..., 199}\n");
-    //test_1();
+    test_1();
     co_yield();
     printf("\n\nTest #2. Expect: (libco-){200, 201, 202, ..., 399}\n");
-    test_2();
+    //test_2();
     printf("\n\n");
 
     return 0;
