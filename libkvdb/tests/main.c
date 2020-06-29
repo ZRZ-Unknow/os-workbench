b908224b3465b15ffb43002235d4cc64d72f9c07
tracer-nju <tracer@nju.edu.cn>
2020-06-30 00:09:07 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 1adcefe45a584d20b08dd6a93cd04e6c   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  00:09:07 up 1 day,  1:55,  1 user,  load average: 2.00, 2.19, 2.39

--- libkvdb/tests/main.c
+++ libkvdb/tests/main.c
@@ -92,7 +92,7 @@ int db_test(){
   value=kvdb_get(db, key); // value = db[key];
   //printf("[%s]: [%s]\n",k,kvdb_get(db,k));
   printf("[%s]: [%s]\n", key, value);
-  free(value);
+  free(value);/*
   for(int i=0;i<10;i++){
     int n1=random(128);
     char *k=gen_string(n1);
@@ -103,7 +103,7 @@ int db_test(){
     assert(0);
     printf("%s\n",p);
     assert(strcmp(p,v)==0);
-  }
+  }*/
   kvdb_close(db);
 }
 
