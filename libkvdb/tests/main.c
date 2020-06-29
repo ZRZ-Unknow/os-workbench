9b70ec008f1613224cf59ee8afca53437e4e20f5
tracer-nju <tracer@nju.edu.cn>
2020-06-30 01:18:44 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 1adcefe45a584d20b08dd6a93cd04e6c   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  01:18:44 up 1 day,  3:05,  1 user,  load average: 1.60, 0.96, 0.91

--- libkvdb/tests/main.c
+++ libkvdb/tests/main.c
@@ -127,11 +127,11 @@ int test2(){
   for(int i=0;i<sizeof(struct r);i++){
     printf("%c",p[i]);
   }
-  printf("%s ",p->flag);
+  /*printf("%s ",p->flag);
   printf("%s ",p->ksize);
   printf("%s ",p->vsize);
   printf("%s ",p->vpos);
-  printf("%s\n",p->key);
+  printf("%s\n",p->key);*/
 }
 
 int main(){
