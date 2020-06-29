55bcaa894497d250fdd6b3242373ad9d31f9553c
tracer-nju <tracer@nju.edu.cn>
2020-06-29 19:24:10 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 1adcefe45a584d20b08dd6a93cd04e6c   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  19:24:11 up 21:10,  1 user,  load average: 1.30, 1.41, 1.47

--- libkvdb/tests/main.c
+++ libkvdb/tests/main.c
@@ -3,16 +3,24 @@
 #include <stdio.h>
 #include <string.h>
 
+char string[27]="abcdefghijklmnopqrstuvwxyz";
+
+char *gen_string(int n){
+  char *str=malloc(n);
+  for(int i=0;i<n-1;i++){
+    str[i]=string[rand(25)];
+  }
+  str[n-1]='\0';
+  return str;
+}
+
+
 int main(){
   struct kvdb *db=kvdb_open("a.db");
   const char *key = "operating-systems";
   char *value;
 
   //panic_on(!(db = kvdb_open("a.db")), "cannot open db"); // 打开数据库
-  char *tmp=malloc(4096);
-  memset(tmp,'p',4096);
-  char *k=malloc(128);
-  memset(k,'t',128);
   //printf("%d,%d\n",strlen(tmp),strlen(k));
   //kvdb_put(db,k,tmp); 
   //kvdb_put(db,"non","ksdjf-385");
@@ -22,5 +30,12 @@ int main(){
   kvdb_close(db); // 关闭数据库
   printf("[%s]: [%s]\n", key, value);
   free(value);
+  for(int i=0;i<50;i++){
+    int n1=rand(128);
+    char *k=gen_string(n1);
+    int n2=rand(4096);
+    char *v=gen_string(n2);
+    kvdb_put(db,k,v);
+  }
   return 0;
 }
