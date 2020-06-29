83ce0b192ec845756dcbce200c2f9e8e28368ee2
tracer-nju <tracer@nju.edu.cn>
2020-06-29 09:52:43 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 1adcefe45a584d20b08dd6a93cd04e6c   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  09:52:43 up 11:38,  1 user,  load average: 1.31, 1.61, 1.44

--- libkvdb/tests/main.c
+++ libkvdb/tests/main.c
@@ -10,11 +10,12 @@ int main(){
 
   //panic_on(!(db = kvdb_open("a.db")), "cannot open db"); // 打开数据库
   char *tmp=malloc(4096);
-  memset(tmp,'k',4096);
+  memset(tmp,'p',4096);
   char *k=malloc(128);
   memset(k,'t',128);
   printf("%d,%d\n",strlen(tmp),strlen(k));
   kvdb_put(db,k,tmp); 
+  kvdb_put(db,"non","ksdjf-385");
   kvdb_put(db, key, tmp); // db[key] = "three-easy-pieces"
   value=kvdb_get(db, key); // value = db[key];
   kvdb_close(db); // 关闭数据库
