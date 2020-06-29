d86c338c14a16eb691113b27b7d1dedad8f47286
tracer-nju <tracer@nju.edu.cn>
2020-06-29 19:38:30 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 1adcefe45a584d20b08dd6a93cd04e6c   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  19:38:31 up 21:24,  1 user,  load average: 1.84, 1.69, 1.56

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -151,7 +151,7 @@ struct kvdb *kvdb_open(const char *filename) {
   strncpy(db->filename,filename,sizeof(db->filename));
   db->committing=0;
   db->start=288+4097*2;
-  flock(db->fd,LOCK_EX);
+  //flock(db->fd,LOCK_EX);
   if(buf.st_size==0){
     for(int i=0;i<2;i++){
       write(db->fd,"0",1);
@@ -189,7 +189,7 @@ struct kvdb *kvdb_open(const char *filename) {
       printf("%s",&c);
     }*/
   }
-  flock(db->fd,LOCK_UN);
+  //flock(db->fd,LOCK_UN);
   return db;
 }
 
@@ -203,7 +203,7 @@ int kvdb_close(struct kvdb *db) {
 
 
 char *kvdb_get(struct kvdb *db, const char *key) {
-  flock(db->fd,LOCK_EX);
+  //flock(db->fd,LOCK_EX);
   for(int i=0;i<(db->size-db->start)/LINESIZE;i++){
     lseek(db->fd,db->start+i*LINESIZE,SEEK_SET);
     char *k=myread(db->fd,0);
@@ -212,10 +212,10 @@ char *kvdb_get(struct kvdb *db, const char *key) {
       continue;
     }
     char *value=myread(db->fd,1);
-    flock(db->fd,LOCK_UN);
+    //flock(db->fd,LOCK_UN);
     return value;
   }
-  flock(db->fd,LOCK_UN);
+  //flock(db->fd,LOCK_UN);
   return NULL;
 }
 
@@ -243,7 +243,7 @@ int journal_put(struct kvdb *db,const char *key,const char *value){
 
 int kvdb_put(struct kvdb *db, const char *key, const char *value) {
   Log("%s,%s",key,value); 
-  flock(db->fd,LOCK_EX);
+  //flock(db->fd,LOCK_EX);
   journal_put(db,key,value);
   if(find_key(db,key)==false){
     lseek(db->fd,0,SEEK_END);
@@ -262,7 +262,7 @@ int kvdb_put(struct kvdb *db, const char *key, const char *value) {
     }
   write(db->fd,"\n",1);
   fsync(db->fd);
-  flock(db->fd,LOCK_UN);
+  //flock(db->fd,LOCK_UN);
   stat(db->filename,&buf);
   db->size=buf.st_size;
   return 0;
