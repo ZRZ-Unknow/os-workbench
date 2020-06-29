19f9353adbf2e264fda26af663c516dae6ee2ccf
tracer-nju <tracer@nju.edu.cn>
2020-06-30 01:18:19 +0800

    Static hostname: zrz          Icon name: computer-laptop            Chassis: laptop         Machine ID: 97ca6b56257b46d58030e191afb9169a            Boot ID: 1adcefe45a584d20b08dd6a93cd04e6c   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-6-amd64       Architecture: x86-64  01:18:19 up 1 day,  3:04,  1 user,  load average: 1.25, 0.87, 0.88

--- libkvdb/tests/main.c
+++ libkvdb/tests/main.c
@@ -124,9 +124,9 @@ int test2(){
   struct r *p=malloc(sizeof(struct r));
   printf("dddd\n");
   read(fd,p,sizeof(struct r));
-  //for(int i=0;i<5;i++){
-  //  printf("%s",tmp[i]);
-  //}
+  for(int i=0;i<sizeof(struct r);i++){
+    printf("%s",p[i]);
+  }
   printf("%s ",p->flag);
   printf("%s ",p->ksize);
   printf("%s ",p->vsize);
